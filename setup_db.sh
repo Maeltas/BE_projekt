101834305bc0e1c63c3a18b56ffe1edab08c061c
Bartosz Głodkowski <glodek@task.gda.pl>
2023-01-20 12:45:49 +0100

    deploy rc1

--- setup_db.sh
+++ setup_db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo docker exec -i BE_165229_DB mysql \
-    -p "BE_165229_DB" -u "root" BE_171884 < ./dump.sql
+sudo docker exec -ti admin-mysql_db.1.0wgtxijosd6k0no4gmlskxlmw mysql \
+    -p "student" -u "root" -Bse "CREATE DATABASE IF NOT EXISTS BE_165229; CREATE USER IF NOT EXISTS 'BE_165229'@'%' IDENTIFIED BY 'BE_165229'; GRANT ALL PRIVILEGES ON BE_165229.* TO 'BE_165229'@'%' WITH GRANT OPTION;"
+sudo docker exec -i admin-mysql_db.1.0wgtxijosd6k0no4gmlskxlmw mysql \
+    -p "BE_165229" -u "BE_165229" BE_165229 < ./dump.sql
